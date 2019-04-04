426b796febe48137107c5b056cb0c18322fedf2e
Nikita Thomas <nikcxz@gmail.com>
2019-04-04 13:49:48 -0400

    heap init setup

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,40 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+
+
+
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        newNode = Node(value)
+        if len(self.storage) == 0:
+            self.storage[0] = newNode
+        else:
+            self.storage.append(newNode)
+            self._bubble_up(len(self.storage)-1)
 
-  def delete(self):
-    pass
 
-  def get_max(self):
-    pass
+    def delete(self):
+        if len(self.storage) == 1:
+            return self.storage.pop()
+        else:
+            firstNode = self.storage.pop(0)
+            lastNode = self.storage.pop(len(self.storage)-1)
+            self.storage[0] = lastNode
+            self._sift_down(0)
+            return firstNode
 
-  def get_size(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def _bubble_up(self, index):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _sift_down(self, index):
-    pass
+    def _bubble_up(self, index):
+        
+
+    def _sift_down(self, index):
+        pass
