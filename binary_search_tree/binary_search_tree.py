75e2b6ace0e49a9628c1cb67422780b69c4a349c
Nikita Thomas <nikcxz@gmail.com>
2019-04-03 22:16:36 -0400

    bst contains and insert

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,43 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        newNode = BinarySearchTree(value)
 
-  def contains(self, target):
-    pass
+        if self.value:
+            if value < self.value:
+                if self.left is None:
+                    self.left = BinarySearchTree(value)
+                else:
+                    self.left.insert(value)
+            elif value > self.value:
+                if self.right is None:
+                    self.right = BinarySearchTree(value)
+                else:
+                    self.right.insert(value)
+        else:
+            self.value = value
 
-  def get_max(self):
-    pass
 
-  def for_each(self, cb):
-    pass
+    def contains(self, target):
+        if target < self.value:
+            if self.value.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+
+        elif target > self.value:
+            if self.value.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
+    def get_max(self):
+        while self.right is not None:
+            
+
+    def for_each(self, cb):
+        pass
